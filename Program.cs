09912716dfb58ffc8d99d7e541a3d01199a65d18
Saurabh Kumar <usersaurabh92@gmail.com>
2020-07-22 19:53:14 +0530

    Basic Setup of test project

---
+++ Program.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Program
+{
+    class Program{
+        static void Main(string[] args){
+            Console.WriteLine("hello saurabh");
+        }
+    }
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,20 @@
+using System;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
